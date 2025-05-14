9aa2c5388f8af03c4580f958f38e283841eeee51
Henry Tito <140865436+HenryTito@users.noreply.github.com>
2025-05-13 21:39:42 -0300

    Update page.js
    Adição do footer.

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@ import Hero from "./components/Hero";
 import AboutSection from "./components/AboutSection";
 import Projects from "./components/Projects";
 import Projetos from "./components/Projetos";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
     <Hero />
     <AboutSection />
     <Projetos />
+    <Footer />
   
     </div>
     </main>
